f1187d2ef8f3a5434fe8d3ccc6e25961de08466c
npaudyal321 <naween321@gmail.com>
2024-01-16 14:49:18 +0545

    django project setup

---
+++ config/settings/env.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-zk!g0of%ysh-kwjv%el0qaxn=&&6^lu)2*+jv3p)1ir3!3at+_'
+
+ALLOWED_HOSTS = ["*"]
+DEBUG = True
